e03c5b0c91e6804341a33a8e34fdad6f3570d69b
Naruby Riverlione <naruby.riverlione@gmail.com>
2016-04-08 22:20:11 +0200

    eslint

--- bot.js
+++ bot.js
@@ -1,17 +1,17 @@
-var _ = require('lodash');
-var debug = require('debug');
-var Slack = require('slack-client');
-var TipBot = require('./lib/tipbot');
-var assert = require('assert');
-var parseArgs = require('minimist');
+var _ = require("lodash");
+var debug = require("debug");
+var Slack = require("slack-client");
+var TipBot = require("./lib/tipbot");
+var assert = require("assert");
+var parseArgs = require("minimist");
 
 var argv = parseArgs(process.argv.slice(2));
 
-var SLACK_TOKEN = argv['slack-token'] || process.env.TIPBOT_SLACK_TOKEN,
+var SLACK_TOKEN = argv["slack-token"] || process.env.TIPBOT_SLACK_TOKEN,
    // BLOCKTRAIL_APIKEY = argv['blocktrail-apikey'] || process.env.TIPBOT_BLOCKTRAIL_APIKEY,
    // BLOCKTRAIL_APISECRET = argv['blocktrail-apisecret'] || process.env.TIPBOT_BLOCKTRAIL_APISECRET,
    // SECRET = argv['secret'] || process.env.TIPBOT_SECRET,
-    TESTNET = argv['testnet'] || process.env.TIPBOT_TESTNET,
+    TESTNET = argv["testnet"] || process.env.TIPBOT_TESTNET,
     AUTO_RECONNECT = true,
     OPTIONS = {ALL_BALANCES: true, DEMAND: true};
 
@@ -51,7 +51,7 @@ Slack.prototype.reconnect = function() {
 
     return setTimeout((function(_this) {
         return function() {
-            _this.logger.info('Attempting reconnect');
+            _this.logger.info("Attempting reconnect");
             return _this.login();
         };
     })(this), timeout);
@@ -60,13 +60,12 @@ Slack.prototype.reconnect = function() {
 var slack = new Slack(SLACK_TOKEN, AUTO_RECONNECT, /* AUTO_MARK */ true);
 var tipbot = new TipBot(slack,TESTNET, OPTIONS);
 
-slack.on('open', function() {
-    var channels = [],
-        groups = [];
+slack.on("open", function() {
+    var channels = [], groups = [];
 
     _.each(slack.channels, function(channel, key) {
         if (channel.is_member) {
-            channels.push('#' + channel.name);
+            channels.push("#" + channel.name);
         }
     });
 
@@ -76,10 +75,10 @@ slack.on('open', function() {
         }
     });
 
-    debug('tipbot:bot')('Connected to Slack, SocketURL: %s', slack.socketUrl);
-    debug('tipbot:bot')('You are <@%s:%s> of %s', slack.self.id, slack.self.name, slack.team.name);
-    debug('tipbot:bot')('You are in (channels): %s', channels.join(', '));
-    debug('tipbot:bot')('As well as (groups): %s', groups.join(', '));
+    debug("tipbot:bot")("Connected to Slack, SocketURL: %s", slack.socketUrl);
+    debug("tipbot:bot")("You are <@%s:%s> of %s", slack.self.id, slack.self.name, slack.team.name);
+    debug("tipbot:bot")("You are in (channels): %s", channels.join(", "));
+    debug("tipbot:bot")("As well as (groups): %s", groups.join(", "));
 
     // init the tipbot
     setTimeout(function() {
@@ -87,16 +86,16 @@ slack.on('open', function() {
     }, 0);
 });
 
-slack.on('message', function(message) {
+slack.on("message", function(message) {
     // debug messages to seperate channel so we only log them when explicitly enabled
-    debug('tipbot:messages')('MESSAGE', message.type, message.channel, message.user, message.text);
+    debug("tipbot:messages")("MESSAGE", message.type, message.channel, message.user, message.text);
 
     var type = message.type,
         channel = slack.getChannelGroupOrDMByID(message.channel),
         member = slack.getUserByID(message.user);
 
     // Respond to messages with the reverse of the text received.
-    if (type === 'message') {
+    if (type === "message") {
         // random stuff we can safely ignore
         if (!message.text || !member) {
             return;
@@ -107,20 +106,20 @@ slack.on('message', function(message) {
     }
 });
 
-slack.on('userChange', function(u) {
+slack.on("userChange", function(u) {
     tipbot.onUserChange(u);
 });
 
-slack.on('close', function(e) {
-    debug('tipbot:bot')('Close!!');
-    console.log(e);
+slack.on("close", function(e) {
+    debug("tipbot:bot")("Close!!" + e);
+    //console.log(e);
 
     process.exit(1);
 });
 
-slack.on('error', function(error) {
-    debug('tipbot:bot')('Error!!');
-    console.log(error);
+slack.on("error", function(error) {
+    debug("tipbot:bot")("Error!!" + error);
+    //console.log(error);
 
     process.exit(1);
 });
