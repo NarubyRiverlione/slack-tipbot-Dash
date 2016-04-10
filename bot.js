af58bad2c70af75e1bff35a15c0278c17084e953
Naruby <naruby.riverlione@gmail.com>
2016-04-10 15:46:34 +0200

    RPC user & password via arguments or environment variables

--- bot.js
+++ bot.js
@@ -7,11 +7,9 @@ var parseArgs = require("minimist");
 
 var argv = parseArgs(process.argv.slice(2));
 
-var SLACK_TOKEN = argv["slack-token"] || process.env.TIPBOT_SLACK_TOKEN,
-    // BLOCKTRAIL_APIKEY = argv['blocktrail-apikey'] || process.env.TIPBOT_BLOCKTRAIL_APIKEY,
-    // BLOCKTRAIL_APISECRET = argv['blocktrail-apisecret'] || process.env.TIPBOT_BLOCKTRAIL_APISECRET,
-    // SECRET = argv['secret'] || process.env.TIPBOT_SECRET,
-    TESTNET = argv["testnet"] || process.env.TIPBOT_TESTNET,
+var SLACK_TOKEN  = argv["slack-token"] || process.env.TIPBOT_SLACK_TOKEN,
+    RPC_USER     = argv["rpc-user"] || process.env.TIPBOT_RPC_USER,
+    RPC_PASSWORD = argv["rpc-password"] || process.env.TIPBOT_RPC_PASSWORD,
     AUTO_RECONNECT = true,
     OPTIONS = {
         ALL_BALANCES: true,
@@ -19,9 +17,9 @@ var SLACK_TOKEN = argv["slack-token"] || process.env.TIPBOT_SLACK_TOKEN,
     };
 
 assert(SLACK_TOKEN, "--slack-token or TIPBOT_SLACK_TOKEN is required");
-//assert(BLOCKTRAIL_APIKEY, "--blocktrail-apikey or TIPBOT_BLOCKTRAIL_APIKEY is required");
-//assert(BLOCKTRAIL_APISECRET, "--blocktrail-apisecret or TIPBOT_BLOCKTRAIL_APISECRET is required");
-//assert(SECRET, "--secret or TIPBOT_SECRET is required");
+assert(RPC_USER, "--rpc-user or RPC_USER is required");
+assert(RPC_PASSWORD, "--rpc-password or TIPBOT_RPC_PASSWORD is required");
+
 
 /**
  * find a DM channel object by userID
@@ -63,7 +61,7 @@ Slack.prototype.reconnect = function() {
 };
 
 var slack = new Slack(SLACK_TOKEN, AUTO_RECONNECT, /* AUTO_MARK */ true);
-var tipbot = new TipBot(slack, TESTNET, OPTIONS);
+var tipbot = new TipBot(slack, RPC_USER, RPC_PASSWORD, OPTIONS);
 
 slack.on("open", function() {
     var channels = [],
