9044c8262e21f21b588cc7311c427be1ade2008e
Naruby <naruby.riverlione@gmail.com>
2016-04-30 21:09:14 +0200

    Added pricelist command as testing for scheduled price information: * bot.js: OPTIONS.PRICE_CHANNEL = name of channel where price information will be showed * tipbot.je: LIST_PRICES = list of  symbols of currencies to be showed

--- bot.js
+++ bot.js
@@ -15,7 +15,8 @@ var WALLET_PASSW = argv["wallet-password"] || process.env.TIPBOT_WALLET_PASSWORD
 
 var OPTIONS = {
     ALL_BALANCES: true,
-    DEMAND: false
+    DEMAND: false,
+    PRICE_CHANNEL: "bot_testing"
 };
 
 assert(SLACK_TOKEN, "--slack-token or TIPBOT_SLACK_TOKEN is required");
@@ -67,10 +68,56 @@ controller.spawn({
     }, 0);
 });
 
-// when bot is connected
+// when bot is connected, show price list in channel 
 controller.on("hello", function (bot, message) {
     debug("tipbot:bot")("BOT CONNECTED: " + message);
+
+    // find channelID of PRICE_CHANNEL to broadcast price messages
+    getPriceChannel(bot, function (err, priceChannel) {
+        if (err) {
+            debug("tipbot:bot")("No price channel to broadcast.");
+        } else {
+            debug("tipbot:bot")("Price channel " + OPTIONS.PRICE_CHANNEL + " = " + priceChannel.id);
+            // tell all prices on the price list
+            tipbot.OPTIONS.PRICE_CHANNEL = priceChannel;
+        }
+    });
 });
+
+function getPriceChannel(bot, cb) {
+ //   var self = this;
+    bot.api.channels.list({}, function (err, channelList) {
+        if (err) {
+            debug("tipbot:bot")("Error retrieving list of channels " + err);
+            cb(err, null);
+        }
+        var priceChannelID = _.filter(channelList.chanels, function (find) {
+            return find.name.match(OPTIONS.PRICE_CHANNEL, "i");
+        });
+
+        if (priceChannelID.length === 1) {
+            cb(null, priceChannelID[0]);
+        } else {
+            debug("tipbot:bot")("Didn't found the " + OPTIONS.PRICE_CHANNEL + ", looking in private groups now.");
+            bot.api.groups.list({}, function (err, groupList) {
+                if (err) {
+                    debug("tipbot:bot")("Error retrieving list of private channels (groups)" + err);
+                    cb(err, null);
+                }
+                var priceGroupID = _.filter(groupList.groups, function (find) {
+                    return find.name.match(OPTIONS.PRICE_CHANNEL, "i");
+                });
+                if (priceGroupID.length === 1) {
+                    cb(null, priceGroupID[0]);
+                } else {
+                    debug("tipbot:bot")("Didn't found the " + OPTIONS.PRICE_CHANNEL + ", in private groups also.");
+                }
+            });
+        }
+
+    });
+}
+
 
 // listen to messages
 controller.hears(".*", ["direct_message", "direct_mention", "mention"], function (bot, message) {
@@ -113,7 +160,7 @@ controller.hears(".*", ["direct_message", "direct_mention", "mention"], function
 controller.on("user_change", function (bot, resp) {
     tipbot.onUserChange(bot, resp.user);
 });
-// TODO test this
+
 // when a new user joins the Slack Team to the user.id can be added
 controller.on("team_join", function (bot, resp) {
     tipbot.onUserChange(bot, resp.user);
@@ -121,16 +168,16 @@ controller.on("team_join", function (bot, resp) {
 
 
 
+
+// ending connection to Slack
 controller.on("close", function (e) {
     debug("tipbot:bot")("Close!!" + e);
-    //console.log(e);
 
     process.exit(1);
 });
 
 controller.on("error", function (error) {
-    debug("tipbot:bot")("Error!!" + error);
-    //console.log(error);
+    debug("tipbot:bot")("Slack Error!!" + error);
 
     process.exit(1);
 });
