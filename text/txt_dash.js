58a1745de5db79ae361787bfbb79ddb9cf95c74c
Naruby Riverlione <--global>
2017-03-11 21:20:19 +0100

    translations

--- text/txt_dash.js
+++ text/txt_dash.js
@@ -8,37 +8,37 @@ const tipbotTxt = {
 
     //'help_balance':
     '*balance*\t\task the bot for your current balance\n' +
-    '\t\t_@DashBot what is my balance_',
+    '\t\t_@mr.robot what is my balance_',
 
     //'help_send':
     '*send*\t\t\t\ttell the bot to send coins to someone; \n' +
-    '\t\t_@DashBot send 0.1 DASH to @someone_ \n' +
+    '\t\t_@mr.robot send 0.1 DASH to @someone_ \n' +
     '\t\t_aliases: give, tip_ \n' +
     '\t\t\t\tWorks also with major fiat currencies (use *currencies* command to see the list); \n' +
-    '\t\t_@DashBot give 4 USD to @someone_',
+    '\t\t_@mr.robot give 4 USD to @someone_',
 
     // 'help_deposit':
-    '*deposit*\t\task the bot for a deposit address; _@DashBot let me deposit!_',
+    '*deposit*\t\task the bot for a deposit address; _@mr.robot let me deposit!_',
 
     // 'help_withdraw':
     '*withdraw*\ttell the bot to withdraw an amount to an address. \n' +
     '\t\t\t\t\tYou can also use _all_ to withdraw your complete balance (minus the tx fee) \n' +
-    '\t\t_@DashBot withdraw 1 DASH to Xdice8EMZmqKvrGE4Qc9bUFf9PX3xaYDp!_',
+    '\t\t_@mr.robot withdraw 1 DASH to Xdice8EMZmqKvrGE4Qc9bUFf9PX3xaYDp!_',
 
     // 'help_currencies':
     '*currencies*\task the bot for a list of supported currencies.\n' +
-    '\t\t_@DashBot what currencies do you know?_ ',
+    '\t\t_@mr.robot what currencies do you know?_ ',
 
     // 'help_price':
     '*price*\t\t\task the bot for the Dash price in a particular currency. Price info from coinmarketcap.\n ' +
-    '\t\t_@DashBot price in USD!_ ',
+    '\t\t_@mr.robot price in USD!_ ',
 
     // 'help_pricelist':
     '*priceticker*\t\tshows all the known prices. Use * priceticker short* for a limited list.',
 
     // 'help_convert':
     '*convert*\t\task the bot to convert between a particular currency and Dash (or visa versa).\n' +
-    '\t\t_@DashBot 0.03 DASH to GBP_ \t or \t _@DashBot 15 EURO to DASH_\n',
+    '\t\t_@mr.robot 0.03 DASH to GBP_ \t or \t _@mr.robot 15 EURO to DASH_\n',
 
     // 'help_rain':
     // '*rain*\t\tcheck the available rain and threshold. \n ' +
@@ -60,15 +60,15 @@ const tipbotTxt = {
   '*balance all*\tshow all the tip jars (must be enabled in code)\n' +
   '\n' +
   '*balance check*\tshow the balance of a specific user (must be enabled in code) \n' +
-  '\t\t_@dashbot balance check @naruby_ \n' +
+  '\t\t_@mr.robot balance check @naruby_ \n' +
   '\n\n' +
   '*whisper*\tSend a message in a private channel to a user as dashbot.\n' +
   '\t\t\t\t\tUse case :moderator warning.\n' +
-  '\t\t_@Dashbot whisper @narbuy stop being silly man._\n' +
+  '\t\t_@mr.robot whisper @narbuy stop being silly man._\n' +
   '\n\n' +
   '*sun threshold*\t set the threshold on where the balance of the sun account will be distributed\n' +
   '\t\t\t\tbetween all the users that tipped. Defaults to 5 Dash.\n' +
-  '\t\t_@dashbot sun threshold 2.5 dash_\n' +
+  '\t\t_@mr.robot sun threshold 2.5 dash_\n' +
   '\n' +
   '*sun eligible*\tSee which users are eligible for a sunray.\n' +
   '\n' +
@@ -77,13 +77,13 @@ const tipbotTxt = {
   '*quiz list*\t\tShow all approved questions.\n' +
   '*quiz review*\tList all questions that need to be reviewed (reward = 0).\n' +
   '*quiz delete _question number_*\tDelete a question.\n' +
-  '*quiz reward _question number_*\tSet/change reward for a question (also approves the question). @dashbot will ask amount.\n' +
+  '*quiz reward _question number_*\tSet/change reward for a question (also approves the question). @mr.robot will ask amount.\n' +
   '*quiz start amount*\t\tStart a quiz by setting the amount of questions.\n' +
   '*quiz abort*\t\tStop a quiz without showing results.\n' +
   '*quiz next*\t\tSkip the current question (if no one finds the answer).\n',
 
   'tx_fee': 'The transaction fee is set to ',
-  'HelpRandom1': 'Here is an example of one of my commands, type "@dashbot help" for my full list. ',
+  'HelpRandom1': 'Here is an example of one of my commands, type "@mr.robot help" for my full list. ',
   // 'HelpRandom2': 'You can get information about all my other cool tricks via the *help* command. \n Have a nice day !',
   // NEW USER
   'WarningNewUser1': 'A new user joins Dash Nation! A warm welcome to our Slack, ',
